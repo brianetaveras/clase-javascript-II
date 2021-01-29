3acfd4d8aa2f8a581e475a27f302517adcd9bdc9
brianetaveras <taveras574@gmail.com>
2021-01-28 20:40:21 -0500

    Agregué la tarea

---
+++ tarea.js
@@ -0,0 +1,146 @@
+
+const saboresOriginales = [
+    "Banana Nut Fudge",
+    "Black Walnut",
+    "Burgundy Cherry",
+    "Butterscotch Ribbon",
+    "Cherry Macaron",
+    "Chocolate",
+    "Chocolate Almond",
+    "Chocolate Chip",
+    "Chocolate Fudge",
+    "Chocolate Mint",
+    "Chocolate Ribbon",
+    "Coffee",
+    "Coffee Candy",
+    "Date Nut",
+    "Eggnog",
+    "French Vanilla",
+    "Green Mint Stick",
+    "Lemon Crisp",
+    "Lemon Custard",
+    "Lemon Sherbet",
+    "Maple Nut",
+    "Orange Sherbet",
+    "Peach",
+    "Peppermint Fudge Ribbon",
+    "Peppermint Stick",
+    "Pineapple Sherbet",
+    "Raspberry Sherbet",
+    "Rocky Road",
+    "Strawberry",
+    "Vanilla",
+    "Vanilla Burnt Almond"
+]
+
+
+/* 
+
+Tarea 1: Copiar un array
+
+Tenemos un Array llamado saboresOriginales con 31 sabores. En estas tareas, estaremos modificando los elementos
+dentro del array. Con todos estos cambios, no queremos perder los 31 sabores originales, así que tenemos que 
+copiar el array original!
+
+*/
+
+
+/* 
+    Utiliza esta función para hacer lo siguiente:
+    1- Recibe un array como parametro (pasaras el array de sabores originales cuando se invoque la función)
+    2- Retorna una copia del array recibido
+*/
+function copiarArray(/* tu código */){
+// tu código
+}
+
+
+
+
+
+/* 
+
+Tarea 2:
+
+El jefe nos ha pedido que agregaras un nuevo sabor al array: Arroz con Ketchup! Debes agregar este nuevo sabor
+a la lista de sabores.
+
+
+Utiliza la función de agregarSabor para hacer lo siguiente:
+    1- Recibir un Array
+    2- Recibir un nuevo sabor cómo un String
+    3- La función agrega el sabor al frente del array que recibes
+    4- La función retorna la array modificada
+
+    ejemplo: agregarSabor(saboresOriginales, "Arroz Con Ketchup")
+
+*/
+
+function agregarSabor(/*tu codigo */){
+    // tu codigo
+}
+
+
+
+/* 
+
+Tarea 3:
+
+El sabor de helado "Aroz con Ketchup" fue un fracaso, quién lo pudo adivinar!? El jefe quiere que sea
+removido de la lista de sabores de inmediato! 
+
+
+Utiliza la función de removerSabor para hacer lo siguiente:
+    1- Recibir un Array
+    2- La función remueve el sabor en frente de la lista
+    3- La función retorna la array modificada
+
+*/
+
+function removerSabor(){
+    // tu codigo
+}
+
+
+/* 
+
+Tarea 4:
+
+Ya que removimos y agregamos sabores, ahora necesitamos hacer inventario de cuantos sabores
+tenemos en nuesta heladería.
+
+
+Utiliza la función de inventarioDeSabores para hacer lo siguiente:
+    1- Recibir un Array
+    2- devolver el número de sabores disponibles en la lista
+
+*/
+
+function inventarioDeSabores(){
+    // tu código
+}
+
+/* 
+
+Tarea 5:
+
+Luego de hacer inventario, nos hemos dado cuenta que no hemos vendido ningún helado! 
+Esto se debe a que los usuarios no tenían acceso a un menú :(
+
+
+Utiliza la función imprimirMenuDeSabores para hacer lo siguiente:
+    1- Recibir un Array
+    2- Crear un loop de 0 al número de elementos en el Array
+    3- Imprime (console.log()) el nombre de cada sabor al lado de su indice
+
+
+    Ejemplo: imprimirMenuDeSabores(['piña', "banana", "paloma"])
+             0 - piña
+             1 - banana
+             2 - paloma
+
+*/
+
+function imprimirMenuDeSabores(/* tu código */){
+    // tu código
+}
